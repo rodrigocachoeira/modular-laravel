593c4a37b8d0f33c7fae036b4342518ced5d932c
Rodrigo Cachoeira <rodrigocachoeira11@gmail.com>
2017-06-20 15:52:25 -0300

    Default stable version

---
+++ app/Core/Factory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Core;
+
+use Illuminate\Database\Eloquent\Factory as MainFactory;
+use Symfony\Component\Finder\Finder;
+
+class Factory extends MainFactory
+{
+
+  public function load ($ptahs = [])
+  {
+    $factory = $this;
+    foreach ($paths as $path) {
+      if (is_dir($path)) {
+        foreach (Finder::create()->files->in($path) as $file) {
+          require $file->getRealPath();
+        }
+      }
+    }
+    return $factory;
+  }
+
+}
---
+++ app/Modules/Admin/Http/Controllers/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Modules\Admin\Http\Controllers;
+
+use App\Core\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+
+  public function index ()
+  {
+    dd('Render index of admin module');
+  }
+
+}
